168c1cccfdd2831df8871746d570d36c74c0f5d4
Makoa <32736507+santarini@users.noreply.github.com>
2019-05-03 16:37:06 -1000

    Create surveyBot.py

---
+++ surveyBot.py
@@ -0,0 +1,21 @@
+import schedule
+import time
+import datetime
+import random
+import pyautogui
+
+def job():
+    timeNow = datetime.datetime.now().strftime("%H:%M:%S")
+    random_num_1 = random.uniform(3,10)
+    random_num_2 = random.uniform(1,10)
+    #wait is done in terms of seconds
+    time.sleep(random_num_1)
+    pyautogui.typewrite("Hey fam do my survey: https://bit.ly/2ZU0mfE\n")
+    time.sleep(random_num_2)
+    pyautogui.typewrite("Trying to figure out what to call my web app\n")
+    print("Completed: " + timeNow + "\n")")
+
+
+schedule.every(1).minutes.do(job)
+#schedule.every().hour.do(job)
+#schedule.every(5).to(10).minutes.do(job)
