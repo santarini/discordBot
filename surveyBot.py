fa42d4b24b8eaf04ce9785585f808df772c718c7
Makoa <32736507+santarini@users.noreply.github.com>
2019-05-03 17:00:06 -1000

    Made it work...

--- surveyBot.py
+++ surveyBot.py
@@ -10,12 +10,15 @@ def job():
     random_num_2 = random.uniform(1,10)
     #wait is done in terms of seconds
     time.sleep(random_num_1)
-    pyautogui.typewrite("Hey fam do my survey: https://bit.ly/2ZU0mfE\n")
+    pyautogui.typewrite("Hey cunt faces do my survey: https://bit.ly/2ZU0mfE\n")
     time.sleep(random_num_2)
     pyautogui.typewrite("Trying to figure out what to call my web app\n")
-    print("Completed: " + timeNow + "\n")")
+    print("Completed: " + timeNow)
 
 
 schedule.every(1).minutes.do(job)
 #schedule.every().hour.do(job)
 #schedule.every(5).to(10).minutes.do(job)
+
+while True:
+    schedule.run_pending()
